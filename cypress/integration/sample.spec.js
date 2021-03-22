46ed427e89afaaaa2d2334fc271aebb176050149
13816025562@139.com <13816025562@139.com>
2021-03-22 23:21:51 +0800

    add cypress

--- cypress/integration/sample.spec.js
+++ cypress/integration/sample.spec.js
@@ -12,8 +12,7 @@ describe('My First Test', () => {
   cy.visit('http://localhost:6006/?path=/story/welcome-ui-input--default')
   // const $li = Cypress.$('#root > div:nth-child(1) > input:nth-child(1)')
 
-  cy.get('#root > div:nth-child(1) > input:nth-child(1)')
-  // cy.get('#storybook-explorer-searchfield').click()
+  cy.get('#root > div:nth-child(1) > input:nth-child(1)').click({force: true})
     .type('fake@email.com')
     .should('have.value', 'fake@email.com')
 })
